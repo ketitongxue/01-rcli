05e84decb4f34daef037be02bad4243e06f85670
ketitongxue <wo_sakura@163.com>
2026-03-21 22:52:37 +0800

    rcli

---
+++ src/opts.rs
@@ -0,0 +1,34 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(name = "rcli", version, author, about, long_about = None)]
+pub struct Opts {
+    #[command(subcommand)]
+    pub cmd: SubCommand,
+}
+
+#[derive(Debug, Parser)]
+pub enum SubCommand {
+    #[command(name = "csv", about = "Show CSV, or Convert CSV to other formats")]
+    Csv(CsvOpts),
+}
+
+#[derive(Debug, Parser)]
+pub struct CsvOpts {
+    #[arg(short, long, help = "Input file", value_parser = verify_input_file)]
+    pub input: String,
+    #[arg(short, long, help = "Output file", default_value = "output.json")]
+    pub output: String,
+    #[arg(short, long, help = "Delimiter", default_value_t = ','.to_string())]
+    pub delimiter: String,
+    #[arg(long, help = "Csv has header or not", default_value_t = true)]
+    pub header: bool,
+}
+
+fn verify_input_file(file: &str) -> Result<String, String> {
+    if std::path::Path::new(file).exists() {
+        Ok(file.into())
+    } else {
+        Err(format!("{} not found", file))
+    }
+}
