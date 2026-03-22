097c6508875ee80644cf149cff763d5299365cc1
ketitongxue <wo_sakura@163.com>
2026-03-22 10:15:27 +0800

    Generalize CSV to JSON conversion

--- src/process.rs
+++ src/process.rs
@@ -1,27 +1,17 @@
-use csv::Reader;
-use serde::{Deserialize, Serialize};
+use csv::{Reader, StringRecord};
 use std::fs;
-
-#[derive(Debug, Deserialize, Serialize)]
-#[serde(rename_all = "PascalCase")]
-struct Player {
-    name: String,
-    #[serde(rename = "Position")]
-    position: String,
-    #[serde(rename = "DOB")]
-    dob: String,
-    #[serde(rename = "Nationality")]
-    nationality: String,
-    #[serde(rename = "Kit Number")]
-    kit: u8,
-}
 
 pub fn process_csv(input: &str, output: &str) -> anyhow::Result<()> {
     let mut rdr = Reader::from_path(input)?;
     let mut ret = Vec::with_capacity(128);
-    for result in rdr.deserialize() {
-        let record: Player = result?;
-        ret.push(record)
+    let headers = rdr.headers()?.clone();
+    for result in rdr.records() {
+        let record: StringRecord = result?;
+        let json_value = headers
+            .iter()
+            .zip(record.iter())
+            .collect::<serde_json::Value>();
+        ret.push(json_value)
     }
 
     let json = serde_json::to_string_pretty(&ret)?;
